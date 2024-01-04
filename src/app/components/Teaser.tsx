7081fe4f78a1354398ac7146a4215c9936f7889e
Eric Schmidt <ericschmidt@Erics-MacBook-Pro.local>
2024-01-04 15:12:05 -0700

    Adds quick fix for broken hover interactions.

--- src/app/components/Teaser.tsx
+++ src/app/components/Teaser.tsx
@@ -1,9 +1,5 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 const Teaser = ({
   title,
@@ -16,57 +12,21 @@ const Teaser = ({
   href: string;
   src: string;
 }) => {
-  const router = useRouter();
-
-  const [focused, setFocused] = useState(false);
-
-  const handleClick =
-    (href: string) => (e: React.MouseEvent<HTMLAnchorElement>) => {
-      // Early return if this click was done via keyboard,
-      // as no additional logic is required (it's hacky 🤷‍♂️).
-      if (e.clientX === 0 && e.clientY === 0) {
-        e.currentTarget.blur();
-        return;
-      }
-
-      // Prevent default behavior of click.
-      e.preventDefault();
-
-      // If the element is already focused and the overlay is visible,
-      // navigate to the link destination, otherwise display the overlay.
-      if (focused) {
-        e.currentTarget.blur();
-        setFocused(false);
-        router.push(href);
-      } else {
-        setFocused(true);
-      }
-    };
-
-  // Remove the focused state when an element is no longer focused,
-  // otherwise it will navigate immediately when re-clicking on an
-  // element that has already been focused once.
-  const handleBlur = (e: React.FocusEvent) => {
-    setFocused(false);
-  };
-
   return (
     <Link
       className="group relative overflow-hidden rounded-sm pointer-cursor border-2 border-black transition-all hover:shadow-[0_0_40px_10px_rgba(255,228,0,0.25)] hover:border-yellow duration-500 ease-in-out cursor-pointer"
       href={href}
-      onClick={handleClick(href)}
-      onBlur={handleBlur}
     >
       <div className="relative aspect-video">
         {/* TODO: blueDataURL doesn't alleviate the pop in on fresh load... */}
         <Image
           alt="boss the cat"
           blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
-          className="object-cover group-focus:grayscale duration-500 ease-in-out"
+          className="object-cover duration-500 ease-in-out"
           src={src}
           fill
         />
-        <div className="absolute top-0 right-0 bottom-0 left-0 p-12 bg-black bg-opacity-90 transition-transform -translate-x-full group-focus:translate-x-0 duration-500 ease-in-out">
+        <div className="absolute top-0 right-0 bottom-0 left-0 p-12 bg-black bg-opacity-90 transition-all group-hover:bg-yellow group-hover:bg-opacity-100 group-hover:text-black group-focus:bg-yellow group-focus:bg-opacity-100 group-focus:text-black duration-500 ease-in-out">
           <h3 className="mb-4 text-3xl leading-tight">{title}</h3>
           <p className="leading-tight">{description}</p>
           <Image
